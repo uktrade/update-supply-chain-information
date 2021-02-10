7397c383de6632550d82eb748c91f8073d1b58da
Lauren <laurenqurashi25@gmail.com>
2021-02-10 15:10:33 +0000

    Add supply chain end point

--- api/api/supply_chain_update/test/test_views.py
+++ api/api/supply_chain_update/test/test_views.py
@@ -3,20 +3,27 @@ import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 
-from api.supply_chain_update.models import StrategicAction
+from api.supply_chain_update.models import StrategicAction, SupplyChain
 from api.supply_chain_update.test.factories import (
     StrategicActionFactory,
     SupplyChainFactory,
 )
+from api.accounts.test.factories import GovDepartmentFactory
 
 
-@pytest.mark.django_db()
-def test_fails_if_unauthenticated():
+@pytest.mark.parametrize(
+    "url_name",
+    (
+        ("supply-chain-list"),
+        ("strategic-action-list"),
+    ),
+)
+def test_fails_if_unauthenticated(url_name):
     """
     Test that a 401 is returned if a request is made to the
-    '/strategic-action' endpoint from an unauthenticated user.
+     endpoints from an unauthenticated user.
     """
-    url = reverse("strategic-action-list")
+    url = reverse(url_name)
     client = APIClient()
     response = client.get(
         url,
@@ -47,7 +54,7 @@ def test_get_all_strategic_actions_from_a_given_supply_chain(
     test_client_with_token,
 ):
     """
-    Test that when a supply chain id is given in query paramters,
+    Test that when a supply chain id is given in query parameters,
     the '/strategic-action' endpoint returns a list of strategic actions
     related to that supply chain.
     """
@@ -56,9 +63,7 @@ def test_get_all_strategic_actions_from_a_given_supply_chain(
     # Create additional strategic action not linked to this supply chain
     StrategicActionFactory()
     client = test_client_with_token
-    response = client.get(
-        "/strategic-actions/", {"supply_chain_id": supply_chain.id}
-    )
+    response = client.get("/strategic-actions/", {"supply_chain_id": supply_chain.id})
     assert response.status_code == 200
     assert len(response.data) == 1
     assert response.data[0]["id"] == str(strategic_action.id)
@@ -74,3 +79,54 @@ def test_all_strategic_actions_returned_if_query_param_empty(
     response = client.get("/strategic-actions/", {"supply_chain_id": ""})
     assert response.status_code == 200
     assert len(response.data) == num_actions
+
+
+@pytest.mark.django_db()
+def test_get_all_supply_chains(
+    test_client_with_token,
+):
+    """
+    Test that all supply chain objects are returned when an
+    authorised request is made to the '/supply-chain' endpoint.
+    """
+    supply_chains = SupplyChainFactory.create_batch(4)
+    num_chains = SupplyChain.objects.count()
+    client = test_client_with_token
+    url = reverse("supply-chain-list")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.data) == num_chains
+    assert response.data[0]["id"] == str(supply_chains[0].id)
+
+
+@pytest.mark.django_db()
+def test_get_all_supply_chain_from_a_given_government_department(
+    test_client_with_token,
+):
+    """
+    Test that when a government department id is given in query parameters,
+    the '/supply-chain' endpoint returns a list of supply chains
+    related to that government department.
+    """
+    gov_department = GovDepartmentFactory()
+    supply_chain = SupplyChainFactory(gov_department=gov_department)
+    # Create additional supply chain that is not linked to this gov department
+    SupplyChainFactory()
+    client = test_client_with_token
+    response = client.get("/supply-chains/", {"gov_department_id": gov_department.id})
+
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0]["id"] == str(supply_chain.id)
+
+
+@pytest.mark.django_db()
+def test_all_supply_chains_returned_if_query_param_empty(
+    test_client_with_token,
+):
+    SupplyChainFactory.create_batch(5)
+    num_chains = SupplyChain.objects.count()
+    client = test_client_with_token
+    response = client.get("/supply-chains/", {"supply_chain_id": ""})
+    assert response.status_code == 200
+    assert len(response.data) == num_chains
--- api/api/urls.py
+++ api/api/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from api.accounts import views as accounts_views
-from api.supply_chain_update.views import StrategicActionViewset
+from api.accounts.views import UserViewSet
+from api.supply_chain_update.views import StrategicActionViewset, SupplyChainViewset
 
 router = routers.DefaultRouter()
-router.register(r"users", accounts_views.UserViewSet, basename="user")
+router.register(r"users", UserViewSet, basename="user")
 router.register(
     r"strategic-actions", StrategicActionViewset, basename="strategic-action"
 )
+router.register(r"supply-chains", SupplyChainViewset, basename="supply-chain")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
