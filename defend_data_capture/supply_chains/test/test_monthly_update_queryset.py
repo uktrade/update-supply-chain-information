6a03aac113fec5044f90839032388a7390106d14
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-04-06 15:37:35 +0100

    Queryset tests.

---
+++ defend_data_capture/supply_chains/test/test_monthly_update_queryset.py
@@ -0,0 +1,42 @@
+import pytest
+from datetime import date
+from django.urls import reverse
+
+from supply_chains.models import (
+    StrategicAction,
+    StrategicActionUpdate,
+    SupplyChain,
+)
+from supply_chains.test.factories import (
+    StrategicActionFactory,
+    StrategicActionUpdateFactory,
+    SupplyChainFactory,
+)
+from accounts.test.factories import GovDepartmentFactory
+
+
+@pytest.mark.django_db()
+def test_last_monthly_update():
+    supply_chain = SupplyChainFactory()
+    strategic_action = StrategicActionFactory(supply_chain=supply_chain)
+    march_strategic_action_update: StrategicActionUpdate = StrategicActionUpdateFactory(
+        strategic_action=strategic_action,
+        supply_chain=strategic_action.supply_chain,
+        submission_date = date(year=2021, month=3, day=31),
+        status=StrategicActionUpdate.Status.SUBMITTED
+    )
+    february_strategic_action_update: StrategicActionUpdate = StrategicActionUpdateFactory(
+        strategic_action=strategic_action,
+        supply_chain=strategic_action.supply_chain,
+        submission_date = date(year=2021, month=2, day=17),
+        status=StrategicActionUpdate.Status.SUBMITTED
+    )
+    january_strategic_action_update: StrategicActionUpdate = StrategicActionUpdateFactory(
+        strategic_action=strategic_action,
+        supply_chain=strategic_action.supply_chain,
+        submission_date = date(year=2021, month=1, day=1),
+        status=StrategicActionUpdate.Status.SUBMITTED
+    )
+    today = date(year=2021, month=4, day=1)
+    last_submitted: StrategicActionUpdate = StrategicActionUpdate.objects.last_submitted_update(today)
+    assert last_submitted.submission_date == march_strategic_action_update.submission_date
