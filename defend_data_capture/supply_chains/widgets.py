a47e7e2a0609096f0e33d8cb3874d66fb1801db5
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-04-07 18:20:30 +0100

    Extract hint and disclosure functionality for RadioSelect widgets into mixins.

--- defend_data_capture/supply_chains/widgets.py
+++ defend_data_capture/supply_chains/widgets.py
@@ -1,11 +1,10 @@
 from django.forms.widgets import RadioSelect
 
 
-class HintedRadioSelect(RadioSelect):
-
-    def __init__(self, attrs=None, choices=(), hints=None):
-        super().__init__(attrs, choices)
-        self.hints: dict = hints
+class HintedSelectMixin:
+    def __init__(self, hints=None, **kwargs):
+        super().__init__(**kwargs)
+        self.hints: dict = {} if hints is None else hints
 
     def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
         option =  super().create_option(name, value, label, selected, index, subindex, attrs)
@@ -14,3 +13,32 @@ class HintedRadioSelect(RadioSelect):
         return option
 
 
+class DisclosingSelectMixin:
+
+    def __init__(self, disclosures=None, **kwargs):
+        super().__init__(**kwargs)
+        self.disclosures = {} if disclosures is None else disclosures
+
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option =  super().create_option(name, value, label, selected, index, subindex, attrs)
+        if value in self.disclosures.keys():
+            option['dit_disclosure'] = self.disclosures[value]
+            try:
+                option['attrs']['data-aria-controls'] = option['attrs']['data-aria-controls'].format(**option['attrs'])
+            except KeyError:
+                pass
+        else:
+            try:
+                option['attrs'].pop('data-aria-controls')
+            except KeyError:
+                pass
+        return option
+
+
+
+class HintedRadioSelect(HintedSelectMixin, RadioSelect):
+    pass
+
+
+class HintedDisclosingRadioSelect(DisclosingSelectMixin, HintedSelectMixin, RadioSelect):
+    pass
