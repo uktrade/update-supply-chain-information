478702d117fb487e18e38b7590252f79573d104c
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-04-08 12:33:56 +0100

    Basic form stuff working.

--- defend_data_capture/supply_chains/forms.py
+++ defend_data_capture/supply_chains/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .widgets import HintedDisclosingRadioSelect
-from .models import StrategicActionUpdate, RAGRatingHints
+from .widgets import HintedDetailRadioSelect
+from .models import StrategicActionUpdate, RAGRatingHints, StrategicAction
 
 
 class MonthlyUpdateInfoForm(forms.ModelForm):
@@ -13,19 +13,33 @@ class MonthlyUpdateInfoForm(forms.ModelForm):
             })
         }
 
-
 class MonthlyUpdateStatusForm(forms.ModelForm):
     class Meta:
         model = StrategicActionUpdate
         fields = ['implementation_rag_rating', 'reason_for_delays']
         widgets = {
-            'implementation_rag_rating': HintedDisclosingRadioSelect(attrs={
+            'implementation_rag_rating': HintedDetailRadioSelect(attrs={
                     'class': 'govuk-radios__input',
-                    'data-aria-controls': "{id}-disclosure"
+                    'data-aria-controls': "{id}-detail"
                 },
                 hints=RAGRatingHints,
-                disclosures = {
-                    'RED': 'Have a banana!'
+                details = {
+                    'RED': {
+                        'template': 'supply_chains/reason_for_delays.html',
+                        'field': lambda self: self['reason_for_delays'],
+                        'prefix': 'red',
+                    },
+                    'AMBER': {
+                        'template': 'supply_chains/reason_for_delays.html',
+                        'field': lambda self: self['reason_for_delays'],
+                        'prefix': 'amber',
+                    },
                 }
             )
         }
+
+
+class MonthlyUpdateTimingForm(forms.ModelForm):
+    class Meta:
+        model = StrategicAction
+        fields = ['target_completion_date']
