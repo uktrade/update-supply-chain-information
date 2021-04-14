5bbd2ecfa47128823fcec12786c8484cf98fa450
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-04-14 16:51:29 +0100

    Remove redundant user_create_mapping method override.

--- defend_data_capture/accounts/auth.py
+++ defend_data_capture/accounts/auth.py
@@ -18,18 +18,7 @@ class CustomAuthbrokerBackend(AuthbrokerBackend):
         id_key = self.get_profile_id_name()
         user_data = {UserModel.USERNAME_FIELD: profile[id_key]}
         user_mapping = self.user_create_mapping(profile)
-        # # if user_mapping["gov_department_id"] is None:
-        # #     return None
         user_data.update(user_mapping)
         email = user_data.pop("email")
         user = UserModel.objects.create_user(email, **user_data)
         return user
-
-    def user_create_mapping(self, profile):
-        # gov_dep_id = get_gov_department_id_from_user_email(profile["email"])
-        return {
-            "email": profile["email"],
-            "first_name": profile["first_name"],
-            "last_name": profile["last_name"],
-            # "gov_department_id": gov_dep_id,
-        }
