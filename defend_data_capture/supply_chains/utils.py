5635722ad1b109a10e08a0036a5964e9556a0e33
Miriam Forner <mforner13@gmail.com>
2021-04-23 12:04:49 +0100

    Add GovDepPermissionMixin for supply chain views
    Checks that the curent user can access the supply chain in the url, and returns a 403 if not.

--- defend_data_capture/supply_chains/utils.py
+++ defend_data_capture/supply_chains/utils.py
@@ -2,7 +2,11 @@ import calendar
 from datetime import date, datetime, timedelta
 
 import holidays
+from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from accounts.models import User
+from supply_chains.models import SupplyChain
 
 
 def get_last_working_day_of_a_month(last_day_of_month: date) -> date:
@@ -38,6 +42,26 @@ class PaginationMixin:
             paged_object = paginator.page(paginator.num_pages)
 
         return paged_object
+
+
+def check_matching_gov_department(user: User, supply_chain: SupplyChain):
+    """Check user's gov department matches that of a supply chain."""
+    return user.gov_department == supply_chain.gov_department
+
+
+class GovDepPermissionMixin:
+    """
+    A mixin which will rasie a 403 forbidden error if
+    a user tries to access a URL for a supply chain not
+    linked to their gov department.
+    """
+
+    def dispatch(self, *args, **kwargs):
+        supply_chain = SupplyChain.objects.get(slug=kwargs.get("sc_slug"))
+
+        if not check_matching_gov_department(self.request.user, supply_chain):
+            raise PermissionDenied
+        return super().dispatch(*args, **kwargs)
 
 
 def get_last_working_day_of_previous_month() -> date:
