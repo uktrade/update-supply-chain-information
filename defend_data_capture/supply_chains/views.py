eaa95423ec334eb47615e0985f44e8c652bf6589
Miriam Forner <mforner13@gmail.com>
2021-04-26 16:53:10 +0100

    Refactor home page view to class based

--- defend_data_capture/supply_chains/views.py
+++ defend_data_capture/supply_chains/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.db.models import Count, QuerySet
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from django.shortcuts import redirect, render
 
 from supply_chains.models import SupplyChain, StrategicAction, StrategicActionUpdate
@@ -21,38 +21,33 @@ from supply_chains.utils import (
 )
 
 
-@login_required
-def index(request):
-    supply_chains = request.user.gov_department.supply_chains.order_by("name")
-    all_supply_chains = supply_chains.annotate(
-        strategic_action_count=Count("strategic_actions")
-    )
+class HomePageView(LoginRequiredMixin, PaginationMixin, ListView):
+    model = SupplyChain
+    context_object_name = "supply_chains"
+    template_name = "index.html"
 
-    deadline = get_last_working_day_of_a_month(get_last_day_of_this_month())
-    last_deadline = get_last_working_day_of_previous_month()
+    def get_queryset(self):
+        return self.request.user.gov_department.supply_chains.annotate(
+            strategic_action_count=Count("strategic_actions")
+        ).order_by("name")
 
-    num_updated_supply_chains = all_supply_chains.submitted_since(last_deadline).count()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        last_deadline = get_last_working_day_of_previous_month()
 
-    page = request.GET.get("page", 1)
-    paginator = Paginator(all_supply_chains, 5)
+        context["supply_chains"] = self.paginate(self.object_list, 5)
+        context["deadline"] = get_last_working_day_of_a_month(
+            get_last_day_of_this_month()
+        )
+        context["num_updated_supply_chains"] = self.object_list.submitted_since(
+            last_deadline
+        ).count()
+        context["gov_department_name"] = self.request.user.gov_department.name
+        context["update_complete"] = (
+            context["num_updated_supply_chains"] == self.object_list.count()
+        )
 
-    try:
-        supply_chains = paginator.page(page)
-    except PageNotAnInteger:
-        supply_chains = paginator.page(1)
-    except EmptyPage:
-        supply_chains = paginator.page(paginator.num_pages)
-
-    template = loader.get_template("index.html")
-
-    context = {
-        "supply_chains": supply_chains,
-        "gov_department_name": request.user.gov_department.name,
-        "deadline": deadline,
-        "num_updated_supply_chains": num_updated_supply_chains,
-        "update_complete": num_updated_supply_chains == all_supply_chains.count(),
-    }
-    return HttpResponse(template.render(context, request))
+        return context
 
 
 class SCSummary(LoginRequiredMixin, TemplateView):
