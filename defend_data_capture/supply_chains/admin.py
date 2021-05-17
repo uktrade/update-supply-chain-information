7e533cc2ee55df6966c1f7da1e8de7c800ad5546
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-05-17 18:00:17 +0100

    Remove admin model registration pending proper implementation on another branch :-)

--- defend_data_capture/supply_chains/admin.py
+++ defend_data_capture/supply_chains/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from accounts.models import GovDepartment
-from supply_chains.models import SupplyChain, StrategicAction, StrategicActionUpdate
-
-admin.site.register(GovDepartment)
-admin.site.register(SupplyChain)
-admin.site.register(StrategicAction)
-admin.site.register(StrategicActionUpdate)
+# Register your models here.
