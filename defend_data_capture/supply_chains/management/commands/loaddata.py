90f16f2fb68d7139498b1d806c9a70c2e3323674
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-05-20 17:48:05 +0100

    Update SAU's created date and last submission date of supply chain for submitted updates.

--- defend_data_capture/supply_chains/management/commands/loaddata.py
+++ defend_data_capture/supply_chains/management/commands/loaddata.py
@@ -3,7 +3,7 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 from django.core.management.commands.loaddata import Command as OriginalCommand
 
-from supply_chains.models import StrategicActionUpdate
+from supply_chains.models import SupplyChain, StrategicActionUpdate
 
 
 class Command(OriginalCommand):
@@ -13,15 +13,23 @@ class Command(OriginalCommand):
         super().handle(*fixture_labels, **options)
         months_to_add = relativedelta(months=self.calculate_months_to_add())
         updates = StrategicActionUpdate.objects.all()
-        self.update_submission_dates(updates, months_to_add)
+        self.update_submission_and_created_dates(updates, months_to_add)
 
-    def update_submission_dates(self, updates, months_to_add):
+    def update_submission_and_created_dates(self, updates, months_to_add):
         updated_updates = []
+        updated_supply_chains = []
         for update in updates:
-            if update.submission_date is not None:
-                update.submission_date += months_to_add
-                updated_updates.append(update)
-        StrategicActionUpdate.objects.bulk_update(updated_updates, ["submission_date"])
+            if update.status == StrategicActionUpdate.Status.SUBMITTED:
+                if update.submission_date is not None:
+                    update.submission_date += months_to_add
+                    update.date_created += months_to_add
+                    update.supply_chain.last_submission_date = update.submission_date
+                    updated_updates.append(update)
+                    updated_supply_chains.append(update.supply_chain)
+        StrategicActionUpdate.objects.bulk_update(
+            updated_updates, ["submission_date", "date_created"]
+        )
+        SupplyChain.objects.bulk_update(updated_supply_chains, ["last_submission_date"])
 
     def calculate_months_to_add(self):
         today = date.today()
