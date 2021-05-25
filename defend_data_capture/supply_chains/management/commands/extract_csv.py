ba86c694433a887c08686b5f9654a8170e47b769
Prashanth Devarajappa <prashanth.devarajappa@digital.trade.gov.uk>
2021-05-25 12:54:36 +0100

    Serialise GovDepartment within extract_csv and tests

--- defend_data_capture/supply_chains/management/commands/extract_csv.py
+++ defend_data_capture/supply_chains/management/commands/extract_csv.py
@@ -7,7 +7,7 @@ from django.core import management
 from django.core.management.commands import dumpdata
 from django.core.management.base import BaseCommand, CommandError
 
-from supply_chains.management.commands.ingest_csv import ALL_MODELS
+from supply_chains.management.commands.ingest_csv import ALL_MODELS, MODEL_GOV_DEPT
 
 
 class Command(BaseCommand):
@@ -45,6 +45,48 @@ class Command(BaseCommand):
 
         return formatted_obj
 
+    def _serialise_gov_department(self, rows: List) -> List:
+        """Serialise model to a generic CSV format
+
+        With email_domains field added as ArrayField to the GovDepartment model, built-in
+        serialiser would leave additional escape chars in the chosen format(CSV)
+        This helper would strip those chars and flatten the list.
+
+        @param List rows: data extracted from chosen model
+        @return: List of rows formatted for CSV
+        """
+        formatted_rows = list()
+        domains_size = 0
+
+        # iterate through the rows,
+        # - Remove escape/meta chars - []""
+        # - Add email_domians_$i field for every domian encountered
+        # - Push largest domains to the top of the list - This will be header for CSV
+        for row in rows:
+            formatted_row = {}
+            for k, v in row.items():
+                if k == "email_domains":
+                    # ["trade.gov.uk", "digital.trade.gov.uk"] -> trade.gov.uk, digital.trade.gov.uk
+                    domains = v[1:-1]
+                    domains = domains.replace('"', "")
+
+                    domain_tokens = domains.split(",")
+
+                    for index in range(len(domain_tokens)):
+                        formatted_row[f"email_domains_{index}"] = domain_tokens[
+                            index
+                        ].strip()
+                else:
+                    formatted_row[k] = v
+
+            if len(domain_tokens) > domains_size:
+                domains_size = len(domain_tokens)
+                formatted_rows.insert(0, formatted_row)
+            else:
+                formatted_rows.append(formatted_row)
+
+        return formatted_rows
+
     def handle(self, **options):
         if options["model"] not in ALL_MODELS:
             raise CommandError(
@@ -64,6 +106,9 @@ class Command(BaseCommand):
                 json_obj = json.loads(status.getvalue())
                 data = self._format_from_django_object(json_obj)
 
+                if options["model"] == MODEL_GOV_DEPT:
+                    data = self._serialise_gov_department(data)
+
                 with open(options["csvfile"], "w", newline="") as fp:
                     if len(data):
                         header = list(data[0].keys())
@@ -71,9 +116,6 @@ class Command(BaseCommand):
                         writer.writeheader()
 
                         writer.writerows(data)
-
-                    else:
-                        pass
 
                 self.stdout.write(
                     self.style.SUCCESS(
