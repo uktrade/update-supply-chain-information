005d7442c9b843c3debbfb8c6b19718cbb11f411
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-05-27 15:26:56 +0100

    Add cache control headers.

---
+++ defend_data_capture/supply_chains/test/test_middleware.py
@@ -0,0 +1,39 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from defend_data_capture import settings
+
+
+def test_correct_middleware_exists():
+    assert settings.MIDDLEWARE == [
+        "django.middleware.security.SecurityMiddleware",
+        "defend_data_capture.middleware.add_cache_control_header_middleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+        "whitenoise.middleware.WhiteNoiseMiddleware",
+        "reversion.middleware.RevisionMiddleware",
+    ]
+
+
+# def test_security_settings_on():
+#     assert settings.SECURE_BROWSER_XSS_FILTER is True
+
+
+class TestCustomMiddleware:
+    def test_custom_cache_control_middleware(self):
+        """
+        Tests that the custom middleware add_cache_control_header_middleware
+        adds the correct cache_control header to a response.
+        """
+        headers = {"HTTP_CONTENT_TYPE": "text/html", "HTTP_ACCEPT": "text/html"}
+        client = Client()
+        response = client.get(reverse("index"), **headers)
+        assert (
+            response["Cache-Control"]
+            == "max-age=0, no-cache, no-store, must-revalidate, private"
+        )
