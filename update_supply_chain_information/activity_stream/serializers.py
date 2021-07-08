a6db11d5af187f7badb1e047bf977cd9bb0ba2f3
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-07-08 14:44:03 +0100

    Serialise models in the format required for Activity Stream.

---
+++ update_supply_chain_information/activity_stream/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+
+
+class ActivityStreamSerializer(serializers.ModelSerializer):
+    department_prefix = "dit"
+    app_name = "UpdateSupplyChainInformation"
+    app_key_prefix = f"{department_prefix}:{app_name}"
+    name = ""
+
+    def _get_user(self, user):
+        pass
+
+    def _get_gov_department(self, gov_department):
+        pass
+
+    def _get_generator(self):
+        """
+        Get a serialized representation of the generator.
+        """
+        return {
+            "name": self.app_key_prefix,
+            "type": "Application",
+            "id": f"{self.app_key_prefix}:ActivityStreamGenerator",
+        }
+
+    def to_representation(self, instance):
+        activity_type = "Announce"
+        object_type = instance["object_type"]
+        object_representation = instance["json"]
+        foreign_keys = instance["foreign_keys"]["keys"]
+        # for some reason, the keys JSON object turns into the string "{}" if empty
+        # so check that it really is a list
+        if isinstance(foreign_keys, list):
+            self.update_foreign_keys(foreign_keys, object_representation)
+        instance_id = object_representation.pop("id")
+        item_id = self.build_item_id(instance_id, object_type)
+        representation = {
+            "id": f"{item_id}:{activity_type}",
+            "name": f"{object_type} {instance_id}",
+            "type": activity_type,
+            "published": instance["last_modified"],
+            "generator": self._get_generator(),
+            "object": {
+                "id": item_id,
+                "type": f"{self.app_key_prefix}:{object_type}",
+            },
+        }
+        representation["object"].update(object_representation)
+        return representation
+
+    def update_foreign_keys(self, foreign_keys, object_representation):
+        for foreign_key, related_object_type in foreign_keys:
+            related_item = object_representation[foreign_key]
+            if related_item:
+                # there is one, or more
+                if isinstance(related_item, str):
+                    # ForeignKey and OneToOne relationships just have a single key
+                    object_representation[foreign_key] = self.build_item_id(
+                        related_item, related_object_type
+                    )
+                else:
+                    # reverse ForeignKey and ManyToMany relationships have multiple keys
+                    object_representation[foreign_key] = [
+                        self.build_item_id(related_item, related_object_type)
+                        for related_item in related_item
+                    ]
+
+    def build_item_id(self, instance_id, object_type):
+        item_id = f"{self.app_key_prefix}:{object_type}:{instance_id}"
+        return item_id
+
+    class Meta:
+        model = None
---
+++ update_supply_chain_information/activity_stream/test/test_serializer.py
@@ -0,0 +1,39 @@
+import pytest
+
+from activity_stream.serializers import ActivityStreamSerializer
+from supply_chains.models import *
+
+pytestmark = pytest.mark.django_db
+
+
+class TestActivityStreamSerializer:
+    def test_serializer_updates_foreign_key(self, strategic_action_queryset):
+        serializer = ActivityStreamSerializer()
+        strategic_action = StrategicAction.objects.order_by("last_modified").first()
+        supply_chain_id = strategic_action.supply_chain_id
+        expected_supply_chain_key = (
+            f"{serializer.app_key_prefix}:{SupplyChain.__name__}:{supply_chain_id}"
+        )
+
+        strategic_action_for_serialisation = (
+            StrategicAction.objects.order_by("last_modified")
+            .for_activity_stream()
+            .first()
+        )
+        representation = serializer.to_representation(
+            strategic_action_for_serialisation
+        )
+
+        assert representation["object"]["supply_chain"] == expected_supply_chain_key
+
+    def test_announcement_id_in_serialisation(self, supply_chain):
+        serializer = ActivityStreamSerializer()
+        queryset = SupplyChain.objects.for_activity_stream()
+        instance = queryset.first()
+
+        representation = serializer.to_representation(instance)
+
+        assert (
+            representation["id"]
+            == f"{serializer.app_key_prefix}:SupplyChain:{supply_chain.pk}:Announce"
+        )
