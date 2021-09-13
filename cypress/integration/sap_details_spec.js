bb96075437737dd21d5d66eb6186b2ca9195e727
Prashanth Devarajappa <prashanth.devarajappa@digital.trade.gov.uk>
2021-09-13 15:03:50 +0100

    Inlude archive info within SAP details for archived actions.

--- cypress/integration/sap_details_spec.js
+++ cypress/integration/sap_details_spec.js
@@ -101,3 +101,74 @@ describe('The SAP details page', () => {
       .should('have.length', 7)
   })
 })
+
+const archivedAction = actions.filter(action => action.fields.name == "SA 007")[0]
+urls.sapDetailArchived = urls.sap + `${govDepartment.name}/` + `${supplyChain.fields.slug}/` + `${archivedAction.fields.slug}/detail/`
+
+describe('The SAP details page for archived action', () => {
+  it('successfully loads', () => {
+    cy.visit(urls.sapDetailArchived)
+  })
+  it("displays user's name and department in header", () => {
+    cy.get('.app-header-item').should(
+      'have.text',
+      `${adminUser.first_name} ${adminUser.last_name} - ${govDepartment.name}`
+    )
+  })
+  it('displays the correct header', () => {
+    cy.get('h1').contains(`Strategic action progress`)
+    cy.get('p').contains('Strategic action progress details and information')
+    cy.get('p').contains(`Apply filters to see strategic action progress`).should('not.exist')
+    cy.get('h2.govuk-heading-l').contains(archivedAction.fields.name)
+  })
+  it('displays navigation links', () => {
+    cy.get('a.govuk-back-link')
+      .contains('Back')
+      .should('have.attr', 'href')
+      .and('equal', urls.sapListing)
+
+    cy.get('a').contains(
+      'Back to top'
+    )
+  })
+  it('display summary table', () => {
+    const summaryHeaders = ['Department', 'Supply chain', 'Strategic action status', 'Strategic action description', 'Start date', 'Estimated date of completion', 'Archive date', 'Reason for archiving']
+    const summaryValues = ['DIT', 'Supply Chain 1', 'Inactive', null, '1 Mar 2009', '31 Dec 2019', '1 Dec 2020', null]
+    cy.get('#action-summary').should('exist')
+    cy.get('#action-summary div.govuk-summary-list__row').should('have.length', 7)
+    cy.get('#action-summary div.govuk-summary-list__row--no-border').should('have.length', 1)
+    cy.get('#action-summary div dd.govuk-summary-list__value')
+      .each(($el, index) => {
+        cy.wrap($el).find('h3.govuk-heading-s').contains(summaryHeaders[index])
+        if (summaryValues[index]) {
+          cy.wrap($el).contains(summaryValues[index])
+        }
+      })
+  })
+  it('display monthly update', () => {
+    const updateHeaders = ['Latest monthly update', 'Delivery status', 'Last updated on']
+    cy.get('#monthly-update').should('exist').should('not.have.attr', 'open')
+    cy.get('#monthly-update summary.govuk-details__summary span.govuk-details__summary-text').contains('Monthly update')
+    cy.get('#monthly-update div.govuk-details__text dl.govuk-summary-list').should('have.length', 1)
+    cy.get('#monthly-update div.govuk-details__text dl.govuk-summary-list div.govuk-summary-list__row').should('have.length', 2)
+    cy.get('#monthly-update div.govuk-details__text dl.govuk-summary-list div.govuk-summary-list__row--no-border').should('have.length', 1)
+    cy.get('#monthly-update div.govuk-details__text dl.govuk-summary-list div dd.govuk-summary-list__value')
+      .should('have.length', 3)
+      .each(($el, index) => {
+        if (index == 2) {
+          cy.wrap($el).contains(updateHeaders[index])
+        } else {
+          cy.wrap($el).find('h3.govuk-heading-s').contains(updateHeaders[index])
+        }
+      })
+  })
+  it('display strategic action details', () => {
+    cy.get('#action-details').should('exist').should('not.have.attr', 'open')
+    cy.get('#action-details summary.govuk-details__summary span.govuk-details__summary-text').contains('Strategic action details')
+    cy.get('#action-details div.govuk-details__text dl.govuk-summary-list').should('have.length', 1)
+    cy.get('#action-details div.govuk-details__text dl.govuk-summary-list div.govuk-summary-list__row').should('have.length', 6)
+    cy.get('#action-details div.govuk-details__text dl.govuk-summary-list div.govuk-summary-list__row--no-border').should('have.length', 1)
+    cy.get('#action-details div.govuk-details__text dl.govuk-summary-list div dd.govuk-summary-list__value')
+      .should('have.length', 7)
+  })
+})
