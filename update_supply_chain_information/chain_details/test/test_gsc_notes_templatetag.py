3f70f3a9f4ae6b382975ff395e05f38e10a318d1
Nick Fitzsimons <nick@nickfitz.co.uk>
2021-09-20 11:23:51 +0100

    Change fixtures into local variables.

--- update_supply_chain_information/chain_details/test/test_gsc_notes_templatetag.py
+++ update_supply_chain_information/chain_details/test/test_gsc_notes_templatetag.py
@@ -7,19 +7,9 @@ from django.template import Context, Template
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture(scope="session")
-def gsc_last_changed_by():
-    return "test"
-
-
-@pytest.fixture(scope="session")
-def gsc_updated_on():
-    return date(year=2021, month=9, day=17)
-
-
-@pytest.fixture(scope="session")
-def gsc_review_on():
-    return date(year=2021, month=9, day=17)
+gsc_last_changed_by = "test"
+gsc_updated_on = date(year=2021, month=9, day=17)
+gsc_review_on = date(year=2021, month=9, day=17)
 
 
 @pytest.fixture
@@ -50,7 +40,7 @@ class TestGSCNotesTemplateTag:
         rendered_template: str = template.render(context)
         assert rendered_template.strip() == ""
 
-    def test_gsc_review_on_returns_only_that(self, gsc_review_on, template):
+    def test_gsc_review_on_returns_only_that(self, template):
         context = Context(
             {
                 "instance": GSCProperties(
@@ -66,7 +56,7 @@ class TestGSCNotesTemplateTag:
             == '<p class="govuk-body">To be reviewed on Friday 17 Sep 2021</p>'
         )
 
-    def test_gsc_updated_on_returns_only_that(self, gsc_updated_on, template):
+    def test_gsc_updated_on_returns_only_that(self, template):
         context = Context(
             {
                 "instance": GSCProperties(
@@ -82,9 +72,7 @@ class TestGSCNotesTemplateTag:
             == '<p class="govuk-body">Last updated on Friday 17 Sep 2021</p>'
         )
 
-    def test_gsc_updated_on_with_gsc_last_changed_by_returns_only_those(
-        self, gsc_last_changed_by, gsc_updated_on, template
-    ):
+    def test_gsc_updated_on_with_gsc_last_changed_by_returns_only_those(self, template):
         context = Context(
             {
                 "instance": GSCProperties(
@@ -100,7 +88,7 @@ class TestGSCNotesTemplateTag:
             == '<p class="govuk-body">Last updated by test on Friday 17 Sep 2021</p>'
         )
 
-    def test_gsc_last_changed_by_returns_only_that(self, gsc_last_changed_by, template):
+    def test_gsc_last_changed_by_returns_only_that(self, template):
         context = Context(
             {
                 "instance": GSCProperties(
