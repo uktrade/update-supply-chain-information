f0ea852bb98fd21424d324abf820a8a52ffc9564
Prashanth Devarajappa <prashanth.devarajappa@digital.trade.gov.uk>
2021-09-24 16:32:47 +0100

    Minor styling and logging changes

--- update_supply_chain_information/supply_chains/management/commands/ingest_stages.py
+++ update_supply_chain_information/supply_chains/management/commands/ingest_stages.py
@@ -1,26 +1,14 @@
 import csv
-import json
-from os import name
-from typing import List, Dict
-from datetime import date, datetime
-import typing
+from typing import Dict
 
-from django.core import management
-from django.core.management.commands import loaddata
-from django.core.management.base import BaseCommand, CommandError
-from django.core.files.temp import NamedTemporaryFile
+from django.core.management.base import BaseCommand
 
 from supply_chains.models import (
     SupplyChain,
-    StrategicAction,
-    StrategicActionUpdate,
     SupplyChainStage,
     SupplyChainStageSection,
 )
-from accounts.models import GovDepartment
 
-
-SC_LIST = r"temp/etl/data/latest_desc.json"
 
 STAGE_NAME_VALUE_LOOKUP = {
     item[1]: item[0] for item in SupplyChainStage.StageName.choices
@@ -32,23 +20,18 @@ SECTION_NAME_VALUE_LOOKUP = {
 
 
 def _update_stage(sc: SupplyChain, row: Dict) -> object:
-    # print(f'SC: {sc.name}\nStage: {row["Stage"]}\nOrder: {row["Order"]}')
     stage, _ = SupplyChainStage.objects.get_or_create(
         supply_chain=sc,
         order=row["Order"],
         name=STAGE_NAME_VALUE_LOOKUP[row["Stage"]],
     )
 
-    # print(stage.get_name_display())
-    # print(stage.order)
     stage.save()
 
     return stage
 
 
 def _update_section(sc: SupplyChain, stage: SupplyChainStage, row: Dict) -> object:
-    # print(f'SC: {sc.name}\nStage: {stage}\nSection: {row["Stage-section"]}')
-
     section, created = SupplyChainStageSection.objects.get_or_create(
         chain_stage=stage,
         name=SECTION_NAME_VALUE_LOOKUP[row["Stage-section"]],
@@ -79,34 +62,45 @@ class Command(BaseCommand):
             reader = csv.DictReader(fp)
             stage_mappings = list(reader)
 
-        print(len(stage_mappings))
-        print("\n")
+        self.stdout.write(
+            self.style.HTTP_INFO(f"\nIngesting {len(stage_mappings)} rows...\n")
+        )
 
         stage_mappings.sort(key=lambda x: x["Supply Chain"])
 
-        for i, row in enumerate(stage_mappings):
-            row["Supply Chain"] = row["Supply Chain"].strip()
-            row["Stage"] = row["Stage"].strip()
+        unknown_chains = set()
+        success_rows = error_count = 0
 
-            try:
-                sc = SupplyChain.objects.get(name=row["Supply Chain"])
-            except SupplyChain.DoesNotExist:
-                print(f'Unknown supply chain : {row["Supply Chain"]}')
-                pass
-            else:
-                print(f'SC: {sc.name}\nStage: {row["Stage"]}\nOrder: {row["Order"]}')
-                stage = _update_stage(sc, row)
-                section = _update_section(sc, stage, row)
+        try:
+            for _, row in enumerate(stage_mappings):
+                row["Supply Chain"] = row["Supply Chain"].strip()
+                row["Stage"] = row["Stage"].strip()
 
-            if i == 100:
-                break
+                try:
+                    sc = SupplyChain.objects.get(name=row["Supply Chain"])
+                except SupplyChain.DoesNotExist:
+                    error_count += 1
+                    unknown_chains.add(row["Supply Chain"])
+                    continue
+                else:
+                    stage = _update_stage(sc, row)
+                    section = _update_section(sc, stage, row)
+                    success_rows += 1
+        except:
+            print(
+                f'Inserting SC: {sc.name}\nStage: {row["Stage"]}\nOrder: {row["Order"]}'
+            )
+            raise
 
-        # unique_scs = sorted(set([x["Supply Chain"] for x in stage_mappings]))
+        self.stdout.write(
+            self.style.SUCCESS(f"{success_rows} rows ingested into the system\n")
+        )
 
-        # with open(SC_LIST, "r") as fp:
-        #     obj = json.load(fp)
-        #     scs = sorted(set(([x["name"] for _, x in obj.items()])))
-
-        # print(set(unique_scs) - set(scs))
-
-        # print(unique_scs)
+        if error_count:
+            self.stdout.write(
+                self.style.HTTP_INFO(
+                    f"\nFailed to ingest {error_count} rows due to below unknown supply chains\n"
+                )
+            )
+            print("\n".join(unknown_chains))
+            print("\n")
