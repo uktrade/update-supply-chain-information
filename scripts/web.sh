d845502d6e7497e004f7c504e7a4b9f22bfd2503
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-05-24 12:21:24 +0100

    Break out the commans to separate lines for readbility

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn config.wsgi:application --config config/gunicorn.py --worker-class gevent --worker-connections 1000 --bind 0.0.0.0:$PORT --timeout 300 --log-file -
+set -e
+
+rootPath="$(dirname "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")"
+
+cd "${rootPath}"
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application --config config/gunicorn.py --worker-class gevent --worker-connections 1000 --bind 0.0.0.0:$PORT --timeout 300 --log-file -
